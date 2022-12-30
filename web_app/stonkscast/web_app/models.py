f5737b54333236574994fed4b41a78010787ab4c
David Hoffmann <david.hoffmann@mail.de>
2022-12-30 23:16:53 +0100

    Integrated Frontend and Django Model

--- web_app/stonkscast/web_app/models.py
+++ web_app/stonkscast/web_app/models.py
@@ -4,5 +4,27 @@ from django.db import models
 class Inference(models.Model):
     stock_name = models.CharField(max_length=20)
     market = models.CharField(max_length=20)
-    kgv = models.FloatField()
-    score = models.FloatField()
+    logo = models.CharField(max_length=200, default=None)
+    score = models.IntegerField()
+
+    def __str__(self) -> str:
+        return f"""
+        Stock name: {self.stock_name} 
+        Market: {self.market}
+        Score: {self.score}
+        Logo: {self.logo}
+        """
+
+    def top_stocks(self) -> dict:
+        return [vars(o) for o in Inference.objects.order_by('-score')]
+
+
+# Import CSV to model
+# df = pd.read_csv("stock_data.csv")[['market', 'logo_url', 'ticker']].dropna()
+# for i in range(10):
+#     Inference(
+#         stock_name=df.iloc[i].ticker,
+#         market=df.iloc[i].market,
+#         logo=df.iloc[i].logo_url,
+#         score=df.iloc[i].score,
+#     ).save()
--- web_app/stonkscast/web_app/views.py
+++ web_app/stonkscast/web_app/views.py
@@ -6,7 +6,8 @@ from .models import Inference
 # Create your views here.
 def index(request):
     template = loader.get_template('web_app/index.html')
+    stocks = Inference()
     context = {
-        'name': 'David',    
+        'top_stocks': stocks.top_stocks()[:6],    
     }
     return HttpResponse(template.render(context, request))
